082c7eea8b90a4275ee40687c31213d9732379ad
Samar Sunkaria <sameesunkaria@me.com>
2018-12-24 20:03:47 +0100

    Initial Commit

---
+++ Beaconing/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Beaconing
+//
+//  Created by Samar Sunkaria on 12/24/18.
+//  Copyright © 2018 samar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Beaconing/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Beaconing
+//
+//  Created by Samar Sunkaria on 12/24/18.
+//  Copyright © 2018 samar. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
